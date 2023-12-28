f35bf71ce95b686b73d4c3d767a4540f61c65292
Raghav Nimbalkar <raghavnimbalkar@Raghavs-MacBook-Air.local>
2023-12-29 04:38:58 +0530

    completed all sections except projects

--- public/index.js
+++ public/index.js
@@ -1,1 +1,20 @@
 
+window.addEventListener('load', function() {
+    const loader = document.querySelector('.loader-wrapper');
+    loader.style.display = 'none'; // Hide the loader once the page is fully loaded
+  });
+  
+
+document.getElementById('#skills-scroll').addEventListener('click', function(e) {
+    e.preventDefault();
+    animateSkills();
+  });
+  
+  function animateSkills() {
+    const skills = document.querySelectorAll('.skill');
+    skills.forEach((skill, index) => {
+      skill.style.animation = `skill-${index + 1} 3s forwards`;
+    });
+  }
+
+
